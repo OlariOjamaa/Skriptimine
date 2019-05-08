dd60b4c727c6044e6ffc2a340e50ad82b5ebf4e8
Olari Ojamaa <olari.ojamaa@khk.ee>
2019-05-08 13:15:05 +0300

    backup

--- praks4a/yl3.sh
+++ praks4a/yl3.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 #
-echo -n "Millisest kaustasts soovite backupi: "; read kaust
-asukoht="/home/user/skriptimine/praks4a/backup/"
-fail=$(echo $kaust >| awk -F/ '{print $NF}')
-date=$(date +"%Y-%m-%d")
-host=$(hostname -s)
-tar -czf $fail.tar.gz $fail
-mv $fail.tar.gz $asukoht/$date-$host-$fail.tar.gz
-echo "Fail $fail on pakitud asukohta $asukoht nime all $date-$host-$fail.tar.gz"
+echo -n "Millist kausta soovtite backupida: "
+read kaust
+echo -n "Kuhu soovite teha backupi: "
+read backup
+tar -czf $kaust.backup.tar.gz $kaust
+mv $kaust.backup.tar.gz $backup
+echo "Kataloogi $kaust backup-i nimi on %kaust.tar.gz ja asub $backup"
 #
